507c061b02ce1919d4f85e9d6e4bcc2b97cf80df
SANJAY-NT <55579860+SANJAY-NT@users.noreply.github.com>
2020-07-15 22:57:12 +0530

    Day 2 Assignment

---
+++ Day 2/scripts/question1.js
@@ -0,0 +1,2 @@
+var customerName = prompt("Please enter your name", "");
+console.log(customerName);
---
+++ Day 2/scripts/question3.js
@@ -0,0 +1,9 @@
+let num = prompt("Enter your age");
+
+if(num<21){
+    console.log("Cannot Drink");
+}
+else
+{
+    console.log("Can Drink");
+}
